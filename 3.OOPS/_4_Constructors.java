2c831b72fe6aec081d04affc10e698e678269bca
Alok955 <deygobindagagan@gmail.com>
2024-06-11 21:31:23 +0530

    constructor

---
+++ 3.OOPS/_4_Constructors.java
@@ -0,0 +1,20 @@
+class Student {
+  String name;
+  int roll;
+
+  // Student(String name) {
+
+  //   System.out.println(name);
+  // }
+  Student() {
+    System.out.println(" constructor is called ....");
+  }
+}
+
+public class _4_Constructors {
+  public static void main(String[] args) {
+    // Student s1 = new Student("alok");
+    Student s1 = new Student();
+  }
+
+}
