935456b469a238fac6124c2a02c79ee589c49ef7
Alok955 <deygobindagagan@gmail.com>
2024-06-22 07:40:36 +0530

    #

---
+++ 2.String/_3_stringMethord.java
@@ -0,0 +1,16 @@
+public class _3_stringMethord {
+  public static void main(String[] args) {
+    String str1 = "Gobinda";
+    String str2 = "Gagan Dey";
+    System.out.println("str1:"+str1);
+    System.out.println("str2:"+str2);
+
+    int lengthOfString = str1.length();
+    System.out.println("length:" + lengthOfString);
+
+    char Char=str1.charAt(0);
+    System.out.println("charAt:"+Char);
+
+  }
+
+}
